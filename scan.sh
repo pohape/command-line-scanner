44da885e21719c77df284401e20d1f8d39209151
Paul Geveiler <pavel@gevejler.ru>
2022-11-30 15:05:57 +0300

    scan.sh

---
+++ scan.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v scanimage)" ]; then
+  echo 'Error: "scanimage" is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v convert)" ]; then
+  echo 'Error: "imagemagic" is not installed.' >&2
+  exit 1
+fi
+
+if [ -z "$1" ]
+then
+    echo "Please specify the output file path."
+    exit 1
+fi
+
+DEVICE=$(scanimage --formatted-device-list "%d" | head -n 1)
+
+if [ -z "$DEVICE" ]
+then
+    echo "No scanning device found. Please make sure you are installed the correct drivers."
+    exit 1
+fi
+
+
+echo "Using device $DEVICE"
+
+TEMP_TIFF=$(mktemp --suffix=.tiff)
+echo "Scanning to a temp file $TEMP_TIFF"
+
+scanimage -d "$DEVICE" --format tiff > $TEMP_TIFF
+
+echo "Converting the image to the specified format and saving it to the file $1"
+convert $TEMP_TIFF $1
+
